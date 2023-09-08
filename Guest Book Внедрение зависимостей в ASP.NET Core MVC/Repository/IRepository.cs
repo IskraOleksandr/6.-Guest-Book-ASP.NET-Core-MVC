d4a32ba53bc31bf4e0e8ef8ad94007baeba99577
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-09-08 22:19:07 +0300

    #Task1 Добавление папки Repository с интерфейсом IRepository.

---
+++ Guest Book Внедрение зависимостей в ASP.NET Core MVC/Repository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Guest_Book_Внедрение_зависимостей_в_ASP.NET_Core_MVC.Models;
+
+namespace Guest_Book_Внедрение_зависимостей_в_ASP.NET_Core_MVC.Repository
+{
+    public interface IRepository
+    {
+        Task<List<Message>> GetMessages();
+
+        Task<User> GetUser(string login);
+
+        Task AddMessage(Message message);
+
+        Task Save();
+
+        Task<List<User>> GetUsers();
+
+        Task AddUser(User user);
+    }
+}
